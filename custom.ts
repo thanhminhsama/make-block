55f87d03c166958451a2a79a8056c567ddfc05f6
thanhminhsama <36581341+thanhminhsama@users.noreply.github.com>
2020-10-31 20:37:43 +0700

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -26,17 +26,28 @@ namespace myBlock {
     //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=4
     //% pin.fieldOptions.tooltips="false"
     export function setFrequency(pin: AnalogPin ,Frequency: number ): void {
-        Frequency = 1000000/Frequency
-        pins.analogSetPeriod(pin, Frequency)
+        let period = 0
+        period = 1000000/Frequency
+        pins.analogSetPeriod(pin, period)
     }
 
-    //% block="Set Frequency|pin %pin| PWM %PWM "
+    //% block="Set PWM|Pin %pin| PWM %PWM "
     //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=4
     //% pin.fieldOptions.tooltips="false"
-    //% PWM.min=0 PWM.max=100
+    //% PWM.min=0 PWM.max=1023
     export function setPWM(pin: AnalogPin ,PWM: number ): void {
-        pins.map(PWM, 0, 100, 0, 1023)
+        
         pins.analogWritePin(pin, PWM)
+
+    }
+    //% block="Khởi tạo"
+
+    export function ConfigurationCar(): void {
+
+        pins.analogWritePin(AnalogPin.P0, 0)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogSetPeriod(AnalogPin.P0, 1000)
+        pins.analogSetPeriod(AnalogPin.P1, 1000)
 
     }
 }
